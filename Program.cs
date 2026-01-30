a28d0f22f864e30f1b66d4cf97689ef5e2561c2b
Prashant Varshney <prashantvarshney4321@gmail.com>
2026-01-30 14:57:49 +0530

    [Prashant] add Program.cs file

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Reflection.AccessPrivateFields;
+using Reflection.DynamicMethodInvocation;
+using Reflection.DynamicObjectCreation;
+using Reflection.GenerateJSON;
+using Reflection.GetClassInfo;
+using Reflection.InvokePrivateMethod;
+using Reflection.MethodExecutionTime;
+using Reflection.ModifyStaticFields;
+using Reflection.RetrieveAttributeAtRuntime;
+namespace Reflection
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            // GetClassInfoMain.Execute();
+            // Demo.Execute();
+            // Invoke.Execute();
+            // CreateObj.Execute();
+            // DynamicMain.Execute();
+            // Retrieve.Execute();
+            // Modify.Execute();
+            // JsonGenerator.Execute();
+            RunnerMain.Execute();
+        }
+    }
+}
